acb5980f7c54e3fddd1f1aab0831ef26d07cd3ef
Mateusz Matkowski <93128950+Castelvill@users.noreply.github.com>
2024-05-24 23:51:47 +0200

    Add recursive search

--- webp_converter.py
+++ webp_converter.py
@@ -9,10 +9,11 @@ if len(sys.argv) >= 2:
 	where = sys.argv[1] + '\\'
 
 webps = glob.glob(where+'*.webp')
+#webps = glob.glob(where+'**/*.webp', recursive=True)
 
 for img in webps:
 	im = Image.open(img).convert("RGB")
-	im.save(img.split(".")[0]+".jpg", jpeg)
+	im.save(img.split(".webp")[0]+".jpg", jpeg)
 	os.remove(img)
 	
 print(f"Updated: {len(webps)}")
